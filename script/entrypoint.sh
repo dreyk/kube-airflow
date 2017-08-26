9daee068f07a61ae0a977c94bb7be6183e744ee8
Alexander Gunin <guninalexander@gmail.com>
2017-08-26 02:54:35 -0700

    new image

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 CMD="airflow"
-cp /tmp/aiflow/airflow-conf.sh $AIRFLOW_HOME/airflow-conf.sh
-chmod +x $AIRFLOW_HOME/airflow-conf.sh
-/tmp/aiflow/airflow-conf.sh > $AIRFLOW_HOME/airflow.cfg
+chmod +x $AIRFLOW_HOME/confgen/airflow-conf.sh
+$AIRFLOW_HOME/confgen/airflow-conf.sh > $AIRFLOW_HOME/airflow.cfg
 exec $CMD "$@"
