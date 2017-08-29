9d6e92f21bfb04e7842ffe0f9849df9a713e6023
Alexander Gunin <guninalexander@gmail.com>
2017-08-29 16:02:03 +0300

    fix conf

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 CMD="airflow"
-chmod +x $AIRFLOW_HOME/confgen/airflow-conf.sh
-$AIRFLOW_HOME/confgen/airflow-conf.sh > $AIRFLOW_HOME/airflow.cfg
+
+/bin/bash $AIRFLOW_HOME/confgen/airflow-conf.sh > $AIRFLOW_HOME/airflow.cfg
+
+cat $AIRFLOW_HOME/airflow.cfg
+
+exit 1
+
 exec $CMD "$@"
